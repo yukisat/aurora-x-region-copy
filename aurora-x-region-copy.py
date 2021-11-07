ced3499a3c21a5a21a32a88a52807de622d03c95
yuki sato (kaba) <35911121+yukisat@users.noreply.github.com>
2021-11-08 08:01:41 +0900

    Update aurora-x-region-copy.py

--- aurora-x-region-copy.py
+++ aurora-x-region-copy.py
@@ -5,18 +5,16 @@ import os
 def lambda_handler(event, context):
     
       rds = boto3.client('rds', region_name='ap-northeast-3')
-      print(event)
+
       message_unicode = event['Records'][0]['Sns']['Message']
       message_dist = json.loads(message_unicode)
       SourceDBClusterSnapshotId = message_dist['Source ARN']
-      print(SourceDBClusterSnapshotId)
       SourceSnapshotName = message_dist['Source ID']
-      print(SourceSnapshotName)
+      SourceSnapshotName = SourceSnapshotName.replace('rds:', '')
       EventId = message_dist['Event ID']
-      print(EventId)
       EventId = EventId.split("#")
       EventId_dist = EventId[1]
-      print(EventId_dist)
+
       if EventId_dist == "RDS-EVENT-0169":
           response = rds.copy_db_cluster_snapshot(
             SourceDBClusterSnapshotIdentifier = SourceDBClusterSnapshotId,
